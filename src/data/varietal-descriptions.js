4964b4c3caec071c32a9b0ab91955761e8abf0da
William Phelps <williamphelps13@gmail.com>
2021-09-27 09:46:46 -0600

    Add varietal descriptions white varietal data

---
+++ src/data/varietal-descriptions.js
@@ -0,0 +1,123 @@
+export const varietalDescriptions = [
+  {
+  name: 'Albarino',
+  color: 'white',
+  acidity: ['medium plus'],
+  body: ['light', 'medium'],
+  tannin: [],
+  oak: 'no',
+  floral: 'yes',
+  minerality: 'yes',
+  similarVarietals: ['Pinot Grigio', 'Riesling', 'Sauvignon Blanc (E)'],
+  pairingPossibilities: ['seafood', 'mussels', 'shrimp', 'roasted vegetables', 'paella']
+  },
+  {
+    name: 'Chardonnay (E)',
+    color: 'white',
+    acidity: ['medium', 'medium plus', 'high'],
+    body: ['medium', 'full'],
+    tannin: [],
+    oak: 'yes',
+    floral: 'no',
+    minerality: 'yes',
+    similarVarietals: ['Chenin Blanc', 'Pinot Grigio'],
+    pairingPossibilities: ['roasted chicken', 'white fish', 'lobster', 'grilled salmon', 'veal']
+  },
+  {
+    name: 'Chardonnay (OE)',
+    color: 'white',
+    acidity: ['low', 'medium minus', 'medium'],
+    body: ['medium', 'full'],
+    tannin: [],
+    oak: 'yes',
+    floral: 'no',
+    minerality: 'no',
+    similarVarietals: ['Albarino', 'Pinot Grigio'],
+    pairingPossibilities: ['grilled salmon', 'crab', 'halibut', 'mushrooms', 'squash']
+  },
+  {
+    name: 'Chenin Blanc',
+    color: 'white',
+    acidity: ['medium plus', 'high'],
+    body: ['light', 'medium'],
+    tannin: [],
+    oak: 'no',
+    floral: 'yes',
+    minerality: 'yes',
+    similarVarietals: ['Chardonnay (E)', 'Pinot Grigio'],
+    pairingPossibilities: ['sauteed fish', 'chinese food', 'seafood', 'grilled trout']
+  },
+  {
+    name: 'Gewurztraminer',
+    color: 'white',
+    acidity: ['low', 'medium minus', 'medium'],
+    body: ['medium', 'full'],
+    tannin: [],
+    oak: 'no',
+    floral: 'yes',
+    minerality: 'yes',
+    similarVarietals: ['Viognier'],
+    pairingPossibilities: ['spicy asian food', 'camembert cheese', 'curries', 'indian food', 'thai food']
+  },
+  {
+    name: 'Pinot Grigio',
+    color: 'white',
+    acidity: ['medium', 'medium plus'],
+    body: ['light', 'medium'],
+    tannin: [],
+    oak: 'no',
+    floral: 'yes',
+    minerality: 'yes',
+    similarVarietals: ['Albarino', 'Chenin Blanc', 'Gewurztraminer', 'Riesling'],
+    pairingPossibilities: ['goat cheese', 'white fish', 'seafood pasta', 'scallops']
+  },
+  {
+    name: 'Riesling',
+    color: 'white',
+    acidity: ['medium plus', 'high'],
+    body: ['light', 'medium'],
+    tannin: [],
+    oak: 'no',
+    floral: 'yes',
+    minerality: 'yes',
+    similarVarietals: ['Albarino'],
+    pairingPossibilities: ['blue cheese', 'curries', 'duck', 'fruit salad', 'baked ham', 'thai food']
+  },
+  {
+    name: 'Sauvignon Blanc (E)',
+    color: 'white',
+    acidity: ['medium plus', 'high'],
+    body: ['light', 'medium'],
+    tannin: [],
+    oak: 'yes',
+    floral: 'yes',
+    minerality: 'yes',
+    similarVarietals: ['Albarino', 'Sauvignon Blanc (OE)'],
+    pairingPossibilities: ['goat cheese', 'oysters', 'smoked salmon']
+  },
+  {
+    name: 'Sauvignon Blanc (OE)',
+    color: 'white',
+    acidity: ['medium plus', 'high'],
+    body: ['light', 'medium'],
+    tannin: [],
+    oak: 'no',
+    floral: 'yes',
+    minerality: 'yes',
+    similarVarietals: ['Sauvignon Blanc (E)'],
+    pairingPossibilities: ['asparagus', 'fried chicken', 'grilled fish', 'grilled pork', 'grilled vegetables', 'turkey']
+  },
+  {
+    name:  'Viognier',
+    color: 'white',
+    acidity: ['medium', 'medium plus'],
+    body: ['full'],
+    tannin: [],
+    oak: 'yes',
+    floral: 'yes',
+    minerality: 'yes',
+    similarVarietals: ['Gewurztraminer'],
+    pairingPossibilities: ['roasted chicken', 'lobster', 'curries', 'roast pork', 'veal']
+  },
+ 
+]
