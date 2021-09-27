4cd430db1239ad2980b419f8740114934aae0e99
William Phelps <williamphelps13@gmail.com>
2021-09-27 15:38:33 -0600

    Add getPairings useEffect and declaration

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -7,6 +7,24 @@ const Recipes = ({ chosenVarietal }) => {
   const [recipeError, setRecipeError] = useState('')
   const [recipes, setRecipes] = useState([])
 
+  useEffect(() => {
+    if (chosenVarietal) {
+      getPairings(chosenVarietal.pairingPossibilities[0])
+    }
+  }, [chosenVarietal])
+
+  const getPairings = (pairingKeyword) => {
+    setRecipes('')
+    setCurrentKeyword(pairingKeyword.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase()))
+
+    getRecipes(pairingKeyword)
+      .then(data => {
+        displayRecipes(data)
+        setRecipeError('')
+      })
+      .catch(error => setRecipeError(error.message))
+  }
+
   
 }
 
