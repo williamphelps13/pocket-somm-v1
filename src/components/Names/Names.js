7aeee16c15665d9b5c84863547bcbcf07e69d139
William Phelps <williamphelps13@gmail.com>
2021-09-27 12:15:38 -0600

    Add imports

---
+++ src/components/Names/Names.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react'
+import './Names.css'
+import { Link } from 'react-router-dom'
+
+const Names = ({ filteredVarietals }) => {
+  
+export default Names
