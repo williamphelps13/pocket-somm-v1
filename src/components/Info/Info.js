4c78bf17ad5bb7822a2adf3f2255cb521b5cb326
William Phelps <williamphelps13@gmail.com>
2021-09-27 15:34:25 -0600

    Add imports

---
+++ src/components/Info/Info.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react'
+import './Info.css'
+import { Link } from 'react-router-dom'
+import { varietalDescriptions } from '../../data/varietal-descriptions'
+import Recipes from '../Recipes/Recipes'
+import Error from '../Error/Error'
+
+const Info = ({ varietalName }) => {
+
+}
+
+export default Info
