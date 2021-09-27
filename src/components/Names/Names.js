fc7ff7720f9b76b23a3df41b734b16b451adda0f
William Phelps <williamphelps13@gmail.com>
2021-09-27 12:16:08 -0600

    Add useEffect torespond to filteredVarietals change

--- src/components/Names/Names.js
+++ src/components/Names/Names.js
@@ -3,5 +3,17 @@ import './Names.css'
 import { Link } from 'react-router-dom'
 
 const Names = ({ filteredVarietals }) => {
-  
+  const [selectedWhites, setSelectedWhites] = useState([])
+  const [selectedReds, setSelectedReds] = useState([])
+
+  useEffect(() => {
+    const displayedWhites = filterLinksColor(filteredVarietals, 'white')
+    const displayedReds = filterLinksColor(filteredVarietals, 'red')
+    
+    setSelectedWhites(displayedWhites)
+    setSelectedReds(displayedReds)
+  }, [filteredVarietals])
+
+}
+
 export default Names
