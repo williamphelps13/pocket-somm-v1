7a051ee9a22545abd39e8b426a46eafcfd27e37a
William Phelps <williamphelps13@gmail.com>
2021-09-27 10:18:41 -0600

    Add imports

---
+++ src/components/App/App.js
@@ -0,0 +1,4 @@
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import { Link, Switch, Route } from 'react-router-dom'
+import { varietalDescriptions } from '../../data/varietal-descriptions'
