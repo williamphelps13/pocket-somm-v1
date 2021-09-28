178c75308b3c0cd06fbe5ef41dd0f6d4d87366a4
William Phelps <williamphelps13@gmail.com>
2021-09-27 19:07:27 -0600

    Remove unneeded switch import

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './App.css'
-import { Switch, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Filter from '../Filter/Filter'
 import Names from '../Names/Names'
 import Info from '../Info/Info'
@@ -11,9 +11,12 @@ const App = () => {
   return (
     <main>
       <h1>Pocket Somm</h1>
+      <p className='purpose'>Serving up wine varietal guidance and food pairings.</p>
+      <p className='key'>E - European countries | OE - Outside Europe</p>
         <Route exact path='/' 
           render={() =>
             <section>
+              <p className='directions'>Select a wine varietal to see immediate pairings or refine your current preferences with our selection guide</p>
               <Names filteredVarietals={filteredVarietals} />
               <Filter setFilteredVarietals={setFilteredVarietals} />
             </section>
