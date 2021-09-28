d8a2418f94f23934ebbcb01cbc612986998f9e2d
William Phelps <williamphelps13@gmail.com>
2021-09-27 21:24:16 -0600

    Add propType for chosenVarietal

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './Recipes.css'
 import { getRecipes } from '../../utilities/api-calls'
+import PropTypes from 'prop-types';
 
 const Recipes = ({ chosenVarietal }) => {
   const [currentKeyword, setCurrentKeyword] = useState('')
@@ -75,3 +76,7 @@ const Recipes = ({ chosenVarietal }) => {
 }
 
 export default Recipes
+
+Recipes.propTypes = {
+  chosenVarietal: PropTypes.object.isRequired
+};
