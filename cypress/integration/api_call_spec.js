25fe9962dac0682baf1aa80e0039184a1ae27c57
William Phelps <williamphelps13@gmail.com>
2021-09-27 21:12:15 -0600

    Add api calls intercept and errors

--- cypress/integration/api_call_spec.js
+++ cypress/integration/api_call_spec.js
@@ -1,21 +1,27 @@
 describe('API Calls Error Messages', () => {
-
-  beforeEach(() => {
-    cy.intercept(`https://api.edamam.com/api/recipes/v2?type=public&q=${pairing}&app_id=533846ff&app_key=%20bc8343dc9b259018b578577a1e32eaaf&field=label&field=image&field=source&field=url&field=yield&field=ingredientLines&field=totalTime`, {
-      statusCode: 201,
+  
+  it('Should be able to display 404 error on chosen varietal page', () => {
+    cy.intercept(`https://api.edamam.com/api/recipes/v2?type=public&q=seafood&app_id=533846ff&app_key=%20bc8343dc9b259018b578577a1e32eaaf&field=label&field=image&field=source&field=url&field=yield&field=ingredientLines&field=totalTime`, {
+      statusCode: 404,
       fixture: 'recipe_test_data.json',
     })
     cy.visit('http://localhost:3000')
-      .get()
+      .get('.0white')
       .click()
+    cy.get('.recipeError')
+      .contains('Recipes not currently available - Destination not found (404 error).')
   })
-  
+
   it('Should be able to display 500 error on chosen varietal page', () => {
-
-  })
-
-  it('Should be able to display 404 error on chosen varietal page', () => {
-
+    cy.intercept(`https://api.edamam.com/api/recipes/v2?type=public&q=seafood&app_id=533846ff&app_key=%20bc8343dc9b259018b578577a1e32eaaf&field=label&field=image&field=source&field=url&field=yield&field=ingredientLines&field=totalTime`, {
+      statusCode: 500,
+      fixture: 'recipe_test_data.json',
+    })
+    cy.visit('http://localhost:3000')
+      .get('.0white')
+      .click()
+    cy.get('.recipeError')
+      .contains('Recipes not currently available - Something on our end is not working right now (500 error).')
   })
 
 })
