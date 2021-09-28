1f9632017c6bc7b49be35b7d028aac3818adcb37
William Phelps <williamphelps13@gmail.com>
2021-09-27 21:12:29 -0600

    Finish chosen varietal tests

--- cypress/integration/chosen_varietal_spec.js
+++ cypress/integration/chosen_varietal_spec.js
@@ -1,12 +1,11 @@
 describe('Chosen Varietal Page', () => {
 
   beforeEach(() => {
-    cy.intercept(`https://api.edamam.com/api/recipes/v2?type=public&q=${pairing}&app_id=533846ff&app_key=%20bc8343dc9b259018b578577a1e32eaaf&field=label&field=image&field=source&field=url&field=yield&field=ingredientLines&field=totalTime`, {
-      statusCode: 201,
-      fixture: 'recipe_test_data.json',
+    cy.intercept('https://api.edamam.com/api/recipes/v2?type=public&q=seafood&app_id=533846ff&app_key=%20bc8343dc9b259018b578577a1e32eaaf&field=label&field=image&field=source&field=url&field=yield&field=ingredientLines&field=totalTime', {
+      fixture: '../fixtures/recipe_test_data.json'
     })
     cy.visit('http://localhost:3000')
-      .get()
+      .get('.0white')
       .click()
   })
 
@@ -30,67 +29,50 @@ describe('Chosen Varietal Page', () => {
   });
 
   it('Should be able to render the Return to All Varietals button', () => {
-  
+    cy.get('.return')
+      .should('exist')
   });
 
   it('Should be able to click the Return to All Varietals button and return to the varietal form page', () => {
-  
+    cy.get('.return')
+      .click()
+    cy.get('.varietalButton')
+      .its('length')
+      .should('eq', 27)
   });
 
   it('Should be able to render the chosen varietal name heading', () => {
-  
-  });
-
-  it('Should be able to render the chosen varietal name heading', () => {
-  
+    cy.get('h2')
+      .contains('Albarino')
   });
 
   it('Should be able to render the chosen varietal color', () => {
-  
+    cy.get('.colorInfo')
+      .contains('Color - white')
   });
 
   it('Should be able to render the chosen varietal typical acidity levels', () => {
-  
+    cy.get('.acidityInfo')
+      .contains('Typical Acidity Levels - medium plus')
   });
 
   it('Should be able to render the chosen varietal typical body', () => {
-  
+    cy.get('.bodyInfo')
+      .contains('Typical Body - light | medium')
   });
 
   it('Should not be able to render the chosen varietal tannin when the varietal is a white wine', () => {
-  
-  });
-
-  it('Should be able to render the chosen varietal similar varietals', () => {
-  
-  });
-
-  it('Should be able to render the chosen varietal favorite pairings', () => {
-  
+    cy.get('.tanninInfo')
+      .should('not.exist')
   });
 
   it('Should be able to render a recipe heading for the chosen varietal', () => {
-  
+    cy.get('.recipeHeading')
+      .contains('Recipes for Seafood -')
   });
 
   it('Should be able to render a recipe card for the chosen varietal', () => {
-  
+    cy.get('.recipeName')
+      .contains('Chicken Vesuvio')
   });
-
-  it('Should be able to render a loading message in the recipe section for the chosen varietal', () => {
-  
-  });
-
-  it('Should be able to click a similar varietal and be directed to that varietals page', () => {
-  
-  });
-
-  it('Should be able to click a different favorite pairing and render a recipe card for that pairing', () => {
-  
-  });
-
-
-
-
-
 })
